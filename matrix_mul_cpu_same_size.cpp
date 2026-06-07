51cd1f225b0820e48a83ee174cbeba4e50d0e71c
niharj2 <jnihar2007@gmail.com>
2026-06-07 22:27:43 +0530

    implemented multiplication assuming 1D inputs asw

--- matrix_mul_cpu_same_size.cpp
+++ matrix_mul_cpu_same_size.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-
+// The input matrices are 2D 
 std::vector<std::vector<int>> matrix_multplication(std::vector<std::vector<int>> M, std::vector<std::vector<int>> N) {
     std::vector<std::vector<int>> product(M.size(), std::vector<int>(M.size())); 
 
@@ -17,4 +17,22 @@ std::vector<std::vector<int>> matrix_multplication(std::vector<std::vector<int>>
     return product;
 }
 
+// Assums all matrices are 1D. This is actually the preferred way and is faster
+std::vector<int> matrix_multplication_1D(std::vector<int> M, std::vector<int>N) {
+    std::vector<int>Product(M.size() * M.size()); 
+
+    for (int i = 0; i < M.size(); ++i) {
+        for (int j = 0; j < N.size(); ++j) {
+            int sum = 0;
+            for (int k = 0; k < N.size(); ++k) {
+                int a = M[i * M.size() + k]; 
+                int b = N[k + N.size() + j];
+                sum += a * b;
+            }
+            Product[i * M.size() + j] = sum;
+        }
+    }
+
+    return Product; 
+}
 
