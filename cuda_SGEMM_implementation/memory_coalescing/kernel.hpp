0bc49ca276632d10f83cae3a2da5f09231b5536e
niharj2 <jnihar2007@gmail.com>
2026-07-28 21:12:32 +0530

    added the memory coalesce kernel

---
+++ cuda_SGEMM_implementation/memory_coalescing/kernel.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cuda_runtime.h>
+
+__global__ void memory_coalesce(
+    int M, 
+    int K, 
+    int N, 
+    float alpha, 
+    float beta, 
+    float* A, 
+    float* B, 
+    float* C
+);
+
+
+__global__ void memory_coalesce_alternative(
+    int M, 
+    int N, 
+    int K, 
+    float alpha, 
+    float beta, 
+    float* A, 
+    float* B, 
+    float*C
+); 
+
